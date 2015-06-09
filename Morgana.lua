1edaf2b2e15c42ff5da2ec81ada55ec9039ae5ea
cttbot <chanhchung89@gmail.com>
2015-06-09 12:36:00 +0700

    GetInfo
    Info

---
+++ Morgana.lua
@@ -0,0 +1,3 @@
+Morgana - FukDa;http://pastebin.com/raw.php?i=97KeJZ3M;s;s;Morgana - FukDa
+Morgana - SXMorgana;https://raw.githubusercontent.com/syraxtepper/bolscripts/master/SXMorgana;s;s;Morgana - SXMorgana
+Morgana - [Serie] Support Master!!!;https://raw.githubusercontent.com/kokosik1221/bol/master/SupportMaster.lua;s;s;Morgana - [Serie] Support Master!!!
