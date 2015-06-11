0298436337940f40fdd330ad7c5342857bb110f8
cttbot <chanhchung89@gmail.com>
2015-06-11 22:29:09 +0700

    GetInfo

--- Jinx.lua
+++ Jinx.lua
@@ -1,4 +1,3 @@
 Jinx - Big Fat;https://raw.githubusercontent.com/boXseven/Big-Fat-Crack/master/Big%20Fat%20Jinx.lua;s;Jinx - Big Fat;s
 Jinx - The Craziest;https://raw.githubusercontent.com/SilentStar/BoLScripts/master/Jinx%20-%20The%20Craziest.lua;s;s;Jinx - The Craziest
 Jinx - Bombs and Bullets;https://raw.githubusercontent.com/RalphLeague/BoL/master/Jinx.lua;s;s;Jinx - Bombs and Bullets
-Jinx - BigFat;https://raw.githubusercontent.com/boXseven/Big-Fat-Crack/master/Big%20Fat%20Jinx.lua;s;Jinx - BigFat;s
--- LeBlanc.lua
+++ LeBlanc.lua
@@ -1,4 +1,3 @@
 LeBlanc - The Prestigious;https://raw.githubusercontent.com/Nickieboy/BoL/master/LeBlanc.lua;s;s;LeBlanc - The Prestigious
 LeBlanc - TSU;https://raw.githubusercontent.com/TsuTsuTsu/TSU-Scripts/master/TSULeBlanc.lua;s;s;LeBlanc - TSU
-LeBlanc - Highlights;https://raw.githubusercontent.com/tungkh1711/bolscript/master/T-Leblanc.lua;s;s;LeBlanc - Highlights
 LeBlanc - Totally;https://dl.dropboxusercontent.com/s/mds47gy90dtzcuj/leban%20ctt.lua?dl=0;LeBlanc - Totally;s;s
