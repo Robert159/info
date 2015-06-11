4bb7cdd1b6a449f95940d1959a3bc31b152e438e
cttbot <chanhchung89@gmail.com>
2015-06-11 21:55:46 +0700

    GetInfo
    fix link

--- ChoGath.lua
+++ ChoGath.lua
@@ -1,3 +1,3 @@
-Cho'Gath - Mythik;ttps://raw.githubusercontent.com/iMythik/BoL/master/MythCho.lua;s;s;Cho'Gath - Mythik
+Cho'Gath - Mythik;https://raw.githubusercontent.com/iMythik/BoL/master/MythCho.lua;s;s;Cho'Gath - Mythik
 Cho'Gath - NomNom To Victory;https://raw.githubusercontent.com/xCrazyGuyNL/BOL/master/CrazyChogath.lua;s;s;Cho'Gath - NomNom To Victory
 Cho'Gath - Silent;https://dl.dropboxusercontent.com/s/sdy5rfbuqvepdn3/Chogath%20Silent.lua?dl=0;s;s;Cho'Gath - Silent
