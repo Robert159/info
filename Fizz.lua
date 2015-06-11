f95843c6c89050daced7a1abcd4cb10232ea4e9b
cttbot <chanhchung89@gmail.com>
2015-06-11 22:05:28 +0700

    GetInfo
    fix

--- Fizz.lua
+++ Fizz.lua
@@ -1,3 +1,3 @@
-Fizz - Fun House;http://www.funhouse.me/download.ashx;s;s;Fizz - Fun House
+Fizz - Fun House;http://www.funhouse.me/download.ashx;s;Fizz - Fun House;s
 Fizz - Fantastik Jungle Steal;https://raw.githubusercontent.com/BoLFantastik/BoL/master/Fantastik%20Fizz%20Jungle%20Steal.lua;s;s;Fizz - Fantastik Jungle Steal
 Fizz - [Serie] Mid Master!!!;https://raw.githubusercontent.com/kokosik1221/bol/master/MidMaster.lua;s;s;Fizz - [Serie] Mid Master!!!
