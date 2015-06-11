843302b31911055cee2abc20b029d156381c4fff
cttbot <chanhchung89@gmail.com>
2015-06-11 21:49:40 +0700

    GetInfo

--- Blitzcrank.lua
+++ Blitzcrank.lua
@@ -1,4 +1,3 @@
-Blitzcrank - Ass-Grabber;https://github.com/AMBER17/BoL/blob/master/Blitzcrank-Ass-Grabber.lua;s;s;Blitzcrank - Ass-Grabber
 Blitzcrank - IronFist;http://dek.land/bol/DeklandAIO.lua;s;Blitzcrank - IronFist;s
 Blitzcrank - [Serie] Support Master!!!;https://raw.githubusercontent.com/kokosik1221/bol/master/SupportMaster.lua;s;s;Blitzcrank - [Serie] Support Master!!!
 Blitzcrank - Q Helper;https://raw.githubusercontent.com/iD3signer/Blitzcrank-Grab-Helper/master/Blitzcrank%20Grab%20Helper.md;s;s;Blitzcrank - Q Helper
