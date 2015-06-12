a3ae7643cdb76e1f4ac6f310d1154af32416af15
cttbot <chanhchung89@gmail.com>
2015-06-12 23:02:28 +0700

    GetInfo
    fix link

--- Draven.lua
+++ Draven.lua
@@ -1,3 +1,2 @@
-Draven - Fantastik;https://dl.dropboxusercontent.com/s/giv1hrhr28maq41/Draven%20Fantastik.lua?dl=0;s;Draven - Fantastik;s
 Draven - Me Crazy;https://raw.githubusercontent.com/jachicao/BoL/master/iAIO.lua;s;s;Draven - Me Crazy
 Draven - Mythik;https://raw.githubusercontent.com/jachicao/BoL/master/iAIO.lua;s;s;Draven - Mythik
