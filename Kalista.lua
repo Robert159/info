8bd63cc530ceebec938012ea973f5a33e8e156a5
cttbot <chanhchung89@gmail.com>
2015-06-12 22:13:25 +0700

    GetInfo

--- Kalista.lua
+++ Kalista.lua
@@ -1,4 +1,4 @@
-Kalista - Kite Machine 2;https://dl.dropboxusercontent.com/s/gpb1mz9y7mfc3ld/kalista%20decode.lua?dl=0;s;Kalista - Kite Machine 2;s
+Kalista - Kite Machine 2;https://raw.githubusercontent.com/cttbot/ScriptsBoL/master/Kite%20Machine%20Kalista.lua;s;Kalista - Kite Machine 2;s
 Kalista - AmberCarries;http://pastebin.com/raw.php?i=wXWzEykJ;s;Kalista - AmberCarries;s
 Kalista - Mythik Elo Shower;https://raw.githubusercontent.com/iMythik/BoL/master/MythKalista.lua;Kalista - Mythik Elo Shower;s;s
 Kalista - Big Fat;https://raw.githubusercontent.com/BigFatNidalee/BoL-Releases/master/Big%20Fat%20Kalista.lua;s;s;Kalista - Big Fat
