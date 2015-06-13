9c2ee12f7db4c4f6095538c5f489921303a16bf7
cttbot <chanhchung89@gmail.com>
2015-06-14 01:25:51 +0700

    GetInfo

--- LeBlanc.lua
+++ LeBlanc.lua
@@ -1,3 +1,3 @@
 LeBlanc - The Prestigious;https://raw.githubusercontent.com/Nickieboy/BoL/master/LeBlanc.lua;s;s;LeBlanc - The Prestigious
 LeBlanc - TSU;https://raw.githubusercontent.com/TsuTsuTsu/TSU-Scripts/master/TSULeBlanc.lua;s;s;LeBlanc - TSU
-LeBlanc - Totally;https://dl.dropboxusercontent.com/s/mds47gy90dtzcuj/leban%20ctt.lua?dl=0;LeBlanc - Totally;s;s
+LeBlanc - Totally;https://raw.githubusercontent.com/cttbot/ScriptsBoL/master/leblanc%20totally.lua;LeBlanc - Totally;s;s
