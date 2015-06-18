c95e82a835d67d572354158c0e80f9ea73d5ccce
cttbot <chanhchung89@gmail.com>
2015-06-18 14:01:37 +0700

    GetInfo

--- Nidalee.lua
+++ Nidalee.lua
@@ -1,6 +1,7 @@
 Nidalee;https://raw.githubusercontent.com/lovehoppang/DPkarthus/master/nidalee.lua;Nidalee;s;s
 Nidalee - The Bestial God;https://raw.githubusercontent.com/DevnBoL/Scripts/master/Nidalee/Nidalee%20-%20The%20Bestial%20God.lua;s;s;Nidalee - The Bestial God
-Nidalee - Beastiality Princess;https://raw.githubusercontent.com/AMBER17/BoL/master/Nidalee.lu;s;s;Nidalee - Beastiality Princess
+Nidalee - Beastiality Princess;https://raw.githubusercontent.com/AMBER17/BoL/master/Nidalee.lua;s;s;Nidalee - Beastiality Princess
 Nidalee - The Nida Lisa;https://raw.githubusercontent.com/LucasRPC/BoL-Scripts/master/The%20Nida%20Lisa.lua;Nidalee - The Nida Lisa;s;s
 Nidalee - Q Helper;https://raw.githubusercontent.com/nebelwolfi/BoL/master/Nida%20Q%20Helper.lua;s;s;Nidalee - Q Helper
 Nidalee - Noscope Reborn;https://raw.githubusercontent.com/dd2repo/BoL/master/NoscopeNidaleeReborn.lua;s;s;Nidalee - Noscope Reborn
+Nidalee - The Beauty and the Beast;https://raw.githubusercontent.com/cttbot/ScriptsBoL/master/Nidalee%20-%20The%20Beauty%20and%20the%20Beast.lua;Nidalee - The Beauty and the Beast;s;s
