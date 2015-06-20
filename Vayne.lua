d523098d24d9eb785de440a684e2ffcbbabf2204
cttbot <chanhchung89@gmail.com>
2015-06-20 12:38:19 +0700

    GetInfo

--- Vayne.lua
+++ Vayne.lua
@@ -1,3 +1,4 @@
 Vayne - AmberCarries;https://raw.githubusercontent.com/boXseven/Cracked/master/Clarity%20Bundle.lua;s;s;Vayne - AmberCarries
 Vayne - The Mighty Hunter;https://raw.githubusercontent.com/Superx321/BoL/master/ShadowVayne.lua;s;s;Vayne - The Mighty Hunter
 Vayne - NewClarity;http://pastebin.com/raw.php?i=rEzips29;s;Vayne - NewClarity;s
+Vayne - Shadow;https://raw.githubusercontent.com/Superx321/BoL/master/ShadowVayne.lua;s;s;Vayne - Shadow
