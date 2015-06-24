b1f5baf0c0841a9f53207b8907d1243604a1210a
cttbot <chanhchung89@gmail.com>
2015-06-24 09:41:57 +0700

    GetInfo

--- Ryze.lua
+++ Ryze.lua
@@ -3,3 +3,5 @@ Ryze - Clarity;https://raw.githubusercontent.com/Nickieboy/BoL/master/ClarityRyz
 Ryze - Fun House;http://www.funhouse.me/download.ashx;s;Ryze - Fun House;s
 Ryze - to the Challenger;http://dek.land/bol/DeklandAIO.lua;s;Ryze - to the Challenger;s
 Ryze - TSU;https://raw.githubusercontent.com/TsuTsuTsu/TSU-Scripts/master/TSURyze.lua;s;s;Ryze - TSU
+Ryze - Smashing;https://raw.githubusercontent.com/nebelwolfi/BoL/master/Ryze.lua;s;s;Ryze - Smashing
+Ryze - Srexi;https://raw.githubusercontent.com/srexi/Srexi-BoL/master/Srexi%20Ryze/Srexi%20Ryze.lua;s;s;Ryze - Srexi
