d520e86bffc6f2a2b24ee0fa41a16ca672f46384
cttbot <chanhchung89@gmail.com>
2015-06-27 17:57:12 +0700

    GetInfo

--- Lissandra.lua
+++ Lissandra.lua
@@ -1,1 +1,2 @@
-Lissandra - Blood Diamond;https://raw.githubusercontent.com/Feez/BoL-Paid/master/Lissandra%20-%20Blood%20Diamond.lua;s;Lissandra - Blood Diamond;s
+Lissandra - iCreative's AIO;https://raw.githubusercontent.com/jachicao/BoL/master/iAIO.lua;s;s;Lissandra - iCreative's AIO
+Lissandra - The Ice Witch;https://raw.githubusercontent.com/jachicao/BoL/master/iAIO.lua;s;s;Lissandra - The Ice Witch
