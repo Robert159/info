2dca9344b01aeb95f49551038a0409aaa3992601
cttbot <chanhchung89@gmail.com>
2015-06-28 17:40:35 +0700

    GetInfo

--- Jinx.lua
+++ Jinx.lua
@@ -1,3 +1,4 @@
 Jinx - Big Fat;https://raw.githubusercontent.com/boXseven/Big-Fat-Crack/master/Big%20Fat%20Jinx.lua;s;Jinx - Big Fat;s
 Jinx - The Craziest;https://raw.githubusercontent.com/SilentStar/BoLScripts/master/Jinx%20-%20The%20Craziest.lua;s;s;Jinx - The Craziest
 Jinx - Bombs and Bullets;https://raw.githubusercontent.com/RalphLeague/BoL/master/Jinx.lua;s;s;Jinx - Bombs and Bullets
+Jinx - Bombs and Bullets 5.11;https://raw.githubusercontent.com/cttbot/ScriptsBoL/master/Jinx%20Bullets%20and%20Bombs%205.11.lua;s;s;Jinx - Bombs and Bullets 5.11
